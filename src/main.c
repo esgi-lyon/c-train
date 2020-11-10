e3ac724bb64208728bbf01d0d3cdac19b606e383
loic-roux <69loloro10@gmail.com>
2020-11-10 16:25:03 +0100

    pointers start

--- src/main.c
+++ src/main.c
@@ -2,6 +2,7 @@
 #include <string.h>
 #include "functions.h"
 #include "types.h"
+#include "pointers.h"
 
 int main(int argc, char **argv)
 {
@@ -9,8 +10,9 @@ int main(int argc, char **argv)
   printf("Address of var is: %p", &p);
   printf("\n%d", Sum(p, 4));
   struct Books Book1;
-  strcpy( Book1.author, "Nuha Ali"); 
+  strcpy(Book1.author, "Nuha Ali"); 
   Book1.id = 1;
+  pointerPlay();
 
   return TRUE;
 }
---
+++ src/pointers.h
@@ -0,0 +1,13 @@
+#ifndef POINTERS_H_INCLUDED
+#define POINTERS_H_INCLUDED
+
+void pointerPlay() 
+{
+    int *p1;
+    double *p2;
+    char *p3;
+    float *p4;
+    printf("\npointer address %p", (void *) &p4);
+}
+
+#endif
