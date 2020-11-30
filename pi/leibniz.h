b16c6cddec1d3c376faec6829a69697753e67821
loic-roux <69loloro10@gmail.com>
2020-11-30 15:50:06 +0100

    doc

--- pi/leibniz.h
+++ pi/leibniz.h
@@ -1,5 +1,5 @@
-#ifndef AMOI_INCLUDED
-#define AMOI_INCLUDED
+#ifndef LEIBNIZ_INCLUDED
+#define LEIBNIZ_INCLUDED
 
 double myPow(int x, int y)
 {
@@ -15,6 +15,7 @@ double Leibniz(int n)
 {
   double pi = 1.0;
   for (int i = 1; i <= n; ++i)
+    // (i & 1) ? -1.0 : 1.0 : Moins Un Puissance N
     pi += ((i & 1) ? -1.0 : 1.0) / ((2 * i) + 1);
 
   return 4 * pi;
--- pi/main.c
+++ pi/main.c
@@ -5,8 +5,6 @@
 
 int main()
 {
-
   for (int n = 10; n <= 100000000; n*=10)
     printf("\t n = %7i : pi= %.14f\n", n, Leibniz(n));
-
 }
